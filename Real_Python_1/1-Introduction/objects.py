eaed3fd25363de18640eee9d5cc35214a42175d9
noelart <noelart@maltanet.net>
2015-04-03 00:17:49 +0200

    update to objects

---
+++ Real_Python_1/1-Introduction/objects.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+
+# Objects & Methods
+
+# upper() method makes uppercase
+name = "Scott"
+message = "Welcome " + name
+print(message.upper())
+
+
+response = input("What shoudl I do")
+response = response.upper()
+print("Well's ", response)
