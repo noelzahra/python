f2d469484b4c26b13c686150ef2583771a810bc1
noelart <noelart@maltanet.net>
2015-04-03 16:32:39 +0200

    strings

---
+++ Real_Python_1/5-Strings/workign_with_strings.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+
+# Strings fundmanetals
+
+# Numbers as strings
+number = "2"
+print(number + number)
+
+anotherNumber = "8"
+print(anotherNumber * 5)
+
+string = "12"
+numberFromString = int(string)
+floatFromString = float(string)
+caption = "String converetd to int: "
+print('========' + "\n" + "String: " + string * 2)
+print(caption, numberFromString * 2)
+print("String converted to float", floatFromString)
