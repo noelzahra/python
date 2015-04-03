b4bbe39bb298d656f9b1c4779003635955a8ba61
noelart <noelart@maltanet.net>
2015-04-03 15:28:36 +0200

    Final introductory parts

---
+++ Real_Python_1/1-Introduction/first_letter.py
@@ -0,0 +1,5 @@
+from __future__ import print_function
+
+user_input = raw_input("tell me your password: ")
+first_letter = user_input[0]
+print("The first letter of your password is: ", first_letter.upper())
---
+++ Real_Python_1/1-Introduction/objects.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+
+# Objects & Methods
+
+# upper() method makes uppercase
+name = "Scott"
+message = "Welcome " + name
+print(message.upper())
+
+
+response = raw_input("What should I do")
+response = response.upper()
+print("You said: ", response.upper())
+
+# capitalize() makes first letter capital
+string = "holiday"
+string = string.capitalize()
+print(string)
+
+anotherString = "easter"
+anotherString = anotherString.upper()
+print(anotherString)
+
+# lowercase with lower() method
+phrase = "NEW YORK"
+phrase = phrase.lower()
+print(phrase)
+
+concat = string + ' ' + anotherString
+print(concat)
+
+# Exrecise
+
+prompt = raw_input("What's your opinion")
+print("You said: ", prompt)
+
+password = "PYTHONCODER"
+password = password.lower()
+print(password)
