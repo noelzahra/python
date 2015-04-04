3761b82c14f0134c61652fa30c47d6abcb78d2d1
noelart <noelart@maltanet.net>
2015-04-04 14:16:34 +0200

    python functions update

--- Real_Python_1/6-Functions_and_loops/exponent.py
+++ Real_Python_1/6-Functions_and_loops/exponent.py
@@ -1,10 +1,10 @@
 from __future__ import print_function
 from __future__ import division
 
-prompt_base = raw_input("Enter base: ")
-promt_exponent = raw_input('Enter exponent: ')
-base = float(prompt_base)
-exponent = float(promt_exponent)
+prompt_base = raw_input("Enter a base: ")
+prompt_exponent = raw_input("Enter an exponent: ")
 
-output = base ** exponent
-print("{} to the power of {} = {}".format(base, exponent, output))
+base = float(prompt_base)
+exponent = float(prompt_exponent)
+ouptut = base ** exponent
+print("{} to the power of {} = {}".format(base, exponent, ouptut))
