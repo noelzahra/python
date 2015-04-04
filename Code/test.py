5d6f22b2feeb4ad06369d834397354eb5eb0c5d0
noelart <noelart@maltanet.net>
2015-04-04 11:15:32 +0200

    Update python test

--- Code/test.py
+++ Code/test.py
@@ -1,5 +1,5 @@
 # Testing #
-from math import floor
+from __future__ import print_function
 
 def is_leap_year(year):
     year = int(year)
@@ -8,5 +8,9 @@ def is_leap_year(year):
         return True
     elif year % 100 == 0 and year % 400 == 0:
         return True
+    else:
+        return False
 
-print(2014)
+year_to_check = is_leap_year(2000)
+
+print(year_to_check)
