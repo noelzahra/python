1b52741c419ee7e3f31835a8b181a2691e9bedb8
noelart <noelart@maltanet.net>
2015-04-08 00:52:45 +0200

    lists

--- Real_Python_1/9-Lists-and-Dicitionaries/lists.py
+++ Real_Python_1/9-Lists-and-Dicitionaries/lists.py
@@ -35,3 +35,61 @@ print(cities)
 # index
 sports = ["running", "swiming", "cyclcing"]
 print(sports.index("cyclcing"))
+
+# copying one list into another
+transport = ["car", "plane", "ship"]
+all_transport = transport
+all_transport.append("motorcycle")
+print(transport)
+
+# copying with extend()
+sites = ["Niaggra falls", "Statue of Liberty", "Mount Rushmore"]
+visit_sites = []
+visit_sites.extend(sites)
+print(visit_sites)
+
+# sortign in alphabetical order
+transport.sort()
+print(transport)
+
+# splitting a string
+ingredients = "eggs, spam, po rocks, soya"
+list = ingredients.split(",")
+print(list)
+
+print("================\n" + "Exercises" + "\n================")
+
+desserts = ["ice cream", "cookies"]
+desserts.sort()
+print(desserts)
+print(desserts.index("ice cream"))
+
+# copy desserts into food
+food = desserts[:]
+print(food)
+
+# Adding 2 items to food
+food.append("broccoli")
+food.append("turnips")
+print(food, desserts)
+
+# remove "cookies"
+food.remove("cookies")
+print(food)
+
+# display the first two items
+print(food[0:2])
+
+# split a string
+string = "cookies, cookies, cookies"
+breakfast = string.split(", ")
+print(breakfast)
+
+
+def list_nums(array):
+    for num in array:
+        if num >= 2 and num <= 20:
+            print(num)
+
+list_nums([2, 4, 8, 16, 32, 64])
+print(list_nums)
