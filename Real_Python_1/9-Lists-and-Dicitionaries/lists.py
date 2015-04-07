f54b0028a1a1d045c6f2583002b3abba79a33324
noelart <noelart@maltanet.net>
2015-04-07 23:37:51 +0200

    Adding lists

--- Real_Python_1/9-Lists-and-Dicitionaries/lists.py
+++ Real_Python_1/9-Lists-and-Dicitionaries/lists.py
@@ -3,15 +3,15 @@ from __future__ import print_function
 # lists are mutable arrays in python
 
 color = ["red", "green", "blue", "yellow"]
-scores = [10, 8, 4, 6]
-mixed_list = ["one", 2, 3.0]
+scores = [2, 4, 6, 8]
+mixed_list = ["car", 3, 4.0]
 
-print(color[2])
+print(color[3])
 print(scores)
-print(scores[1:3])
+print(mixed_list[1:3])
 
-color[0] = "burgundy"
-color[3] = "indigo"
+color[0] = "orange"
+color[3] = "black"
 print(color)
 
 # append method
@@ -29,9 +29,9 @@ print(animals)
 # extend method
 cities = []
 cities.append("New York")
-cities.extend(["San Francisco", "Boston", "Chicago"])
+cities.extend(["San Francisco", "Chicago", "Boston"])
 print(cities)
 
 # index
-sports = ["running", "swimming", "cycling", "climbing"]
-print(sports.index("cycling"))
+sports = ["running", "swiming", "cyclcing"]
+print(sports.index("cyclcing"))
