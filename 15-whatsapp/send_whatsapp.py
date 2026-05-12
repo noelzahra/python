8189fbecd8e8fae6296ee89ce57e9fa02614303e
noelzahra <noelzahra@icloud.com>
2026-05-12 22:37:18 +0200

    Refactor send_whatsapp function

--- 15-whatsapp/send_whatsapp.py
+++ 15-whatsapp/send_whatsapp.py
@@ -5,25 +5,37 @@ import json
 from datetime import datetime
 
 load_dotenv()
+
 TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
 TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
 DCU_PHONE_NUMBER = os.getenv('DCU_PHONE_NUMBER')
 CONTENT_SID = os.getenv('CONTENT_SID')
 
-now = datetime.now().strftime("%A %d/%m/%Y %H:%M:%S")
-timestamp = f"Message sent on {now}"  
 
-client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+def send_whatsapp(to: str = DCU_PHONE_NUMBER, body: str = None, content_sid: str = CONTENT_SID, content_variables: dict = None) -> str:
+    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
-message = client.messages.create(
-        from_=TWILIO_PHONE_NUMBER,
-        to =DCU_PHONE_NUMBER,
-        content_sid=CONTENT_SID,
-        content_variables=json.dumps(
-            {
-            "1": timestamp
+    kwargs = {
+        "from_": TWILIO_PHONE_NUMBER,
+        "to": to,
+    }
+
+    if content_sid:
+        now = datetime.now().strftime("%A %d/%m/%Y %H:%M:%S")
+        default_variables = {
+            "name"     :"Noel",
+            "payload"     : f"Message sent on {now}"
             }
-        )
-)
-print(f"Message content: {message.body}")
+        kwargs["content_sid"] = content_sid
+        kwargs["content_variables"] = json.dumps(content_variables or default_variables)
+    elif body:
+        kwargs["body"] = body
+
+    message = client.messages.create(**kwargs)
+    print(f"Message sent: {message.sid}")
+    return message.sid
+
+
+if __name__ == "__main__":
+    send_whatsapp()
