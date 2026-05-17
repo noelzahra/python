49d18def9bc0db4a6e10c4de079a003e383d0a81
noelzahra <noelzahra@icloud.com>
2026-05-17 17:57:16 +0200

    Add datetime formats

---
+++ 18-datetime/date_formats.py
@@ -0,0 +1,34 @@
+# Using satettime library
+
+from datetime import datetime, tzinfo
+
+now = datetime.now()
+year = datetime.now().year
+month = datetime.now().month
+print(f"timestamp: {now}, Get year: {year}, Get month: {month}")
+
+formatted_time = now.strftime("Today: %A date:%d/%m/%Y time: %H:%M:%S")
+print(f"Formatted time: {formatted_time}")
+
+daybatch = {
+     'Log Id': '707022',
+     'Log date': '5/14/20267:44:57 AM',
+     'Instrument': 'ICTHouseholds2026',
+     'Key': '',
+     'Interviewer': 'ICTSup1',
+     'Action': 'Create daybatch',
+     'Success': 'Yes',
+     'Message': "End creating daybatch for 5/14/2026. Number of included/processed records: 1230/2460. 1185 cases were excluded with reason: 'CaseCompleted'. 39 cases were excluded with reason: 'WrongDay'. 6 cases were excluded with reason: 'NumberOfCalls'. Cati Server Time Zone: W. Europe Standard Time (offset 01:00:00). DoNotCallBefore: 421, DoNotCallAfter: 1261. Found 1 TimeZones: CET (W. Europe Standard Time). Cases found: 2460: added 0 new records to Case Info, updated 2460 existing records in Case Info. TimeZone CET: DoNotCallBefore is 5/14/2026 7:00:00 AM (DayOffset: 0). DoNotCallAfter is 5/14/2026 9:00:00 PM (DayOffset: 0)."
+    }
+
+def get_log_date(daybatch):
+    for item in daybatch:
+        if item == 'Log date':
+            #regex string to date
+            log_date = datetime.strptime(daybatch[item], '%m/%d/%Y%I:%M:%S %p')
+
+            #format date to string
+            log_date_formatted = log_date.strftime('%d-%m-%Y %H:%M:%S %p') 
+            return log_date_formatted
+
+print(f"Log date: {get_log_date(daybatch)}")
