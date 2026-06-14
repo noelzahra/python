bab80b293bdba184d5a297677f1f937977ffd27d
noelzahra <noelzahra@icloud.com>
2026-06-14 13:45:36 +0200

    Added Series methods for counting

--- 19-more_pandas/1-foundations/movies_lookup.py
+++ 19-more_pandas/1-foundations/movies_lookup.py
@@ -6,7 +6,9 @@
     4. A Series can support method chaining. Series.method1().method2()...methodN()
 
 Dataframe: A table of rows and columns
-    1. A DataFrame has three main components: data (referred as values), index, and columns, which are all mutable. 
+    1. A DataFrame has three main components: data (referred as values), index, and columns, which are all mutable.
+
+url: https://github.com/PacktPublishing/Pandas-Cookbook/tree/master
 """
 
 import pandas as pd
@@ -15,6 +17,7 @@ import os
 # movies is a DataFrame
 movies = pd.read_csv(os.path.join("..", "data", "movie.csv"))
 print([movies.head(), movies.tail()])
+
 
 # director_name is a Series, one column of the DataFrame movies
 director_name = movies['director_name']
@@ -27,9 +30,27 @@ print(director_name.head())
 genre = movies['genres']
 print(genre.head())
 
+
+"""
+COUNTING VALUES IN A SERIES
+    Counting values in a Series can be done using the Series method count() or value_counts()
+"""
 # Count values in a Series
 directors_count = director_name.count()
 print(f"Number of directors: {directors_count}")
 
 genre_count = genre.count()
 print(f"Number of genres: {genre_count}" )
+
+print(movies['genres'].count())  # Similar as variable genre_count
+
+#Series method value_counts() returns a Series containing counts of unique values in the Series
+print(movies['director_name'].value_counts())
+
+#len() function can also be used to count the number of values in a Series
+number_of_directors = len(director_name)
+size_of_directors = director_name.size
+print(f"Number of directors using len(): {number_of_directors} Number of directiors using size: {size_of_directors}")
+
+
+
